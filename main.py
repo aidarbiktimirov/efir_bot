f03f2f6f70f00e5649d22fb519169e10466988ed
Aydar Biktimirov <aydar@biktimirov.net>
2015-09-26 00:45:24 +0300

    Reply changes

--- main.py
+++ main.py
@@ -17,34 +17,42 @@ class UserState(object):
 
 
 def get_upcoming_event(user_id):
-    events = [event for event in db_wrapper.Event.get_upcoming_events() if db_wrapper.Vote(user_id, event.event_id).predicted_score is None]
-    return events[0] if events else None
+    try:
+        events = [event for event in db_wrapper.Event.get_upcoming_events() if db_wrapper.Vote(user_id, event.event_id).predicted_score is None]
+        return events[0] if events else None
+    except Exception as e:
+        print e
 
 
 @bot.message_handler(commands=['vote'])
 def vote_handler(message):
-    event = get_upcoming_event(message.chat.id)
+    event = get_upcoming_event(message.from_user.id)
     if event is None:
-        bot.send_message(message.chat.id, 'There are not upcoming events to vote for. {}'.format(telegram.Emoji.CRYING_CAT_FACE))
+        bot.reply_to(message, 'There are no upcoming events for you to vote for {}'.format(telegram.Emoji.CRYING_CAT_FACE))
         return
-    bot.send_message(message.chat.id, 'Vote for upcoming match {}'.format(event.name))
-    user_states[message.chat.id] = UserState.WAITING_FOR_VOTE
-    user_events[message.chat.id] = event.event_id
+    bot.reply_to(message, 'Vote for upcoming match {}'.format(event.name))
+    user_states[message.from_user.id] = UserState.WAITING_FOR_VOTE
+    user_events[message.from_user.id] = event.event_id
 
 
-@bot.message_handler(func=lambda message: user_states.get(message.chat.id, UserState.UNKNOWN) == UserState.WAITING_FOR_VOTE)
+@bot.message_handler(func=lambda message: user_states.get(message.from_user.id, UserState.UNKNOWN) == UserState.WAITING_FOR_VOTE)
 def handle_vote(message):
     predictions = [word.split(':') for word in message.text.split() if ':' in word]
     try:
         assert predictions and len(predictions[0]) == 2
         a, b = int(predictions[0][0]), int(predictions[0][1])
     except:
-        bot.reply_to(message, 'Please fix your vote, it\'s incorrect')
+        bot.reply_to(message, 'Please fix your vote, it\'s incorrect {}'.format(telegram.Emoji.ANGRY_FACE))
         return
-    bot.reply_to(message, 'Thank you for your vote!')
-    db_wrapper.Vote(message.chat.id, user_events.get(message.chat.id)).set_score('{}:{}'.format(a, b))
-    user_states[message.chat.id] = UserState.UNKNOWN
-    del user_events[message.chat.id]
+    bot.reply_to(message, 'Thank you for your vote {}!'.format(message.from_user.first_name))
+    db_wrapper.Vote(message.from_user.id, user_events.get(message.from_user.id)).set_score('{}:{}'.format(a, b))
+    user_states[message.from_user.id] = UserState.UNKNOWN
+    del user_events[message.from_user.id]
+
+
+@bot.message_handler()
+def handle_other_messages(message):
+    bot.reply_to(message, 'No poll is currently active for you, maybe you want to start one by typing /vote?')
 
 
 if __name__ == '__main__':
