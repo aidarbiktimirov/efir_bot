ed2e13a2a3bd697239f3b8f857f1ed4ea582b0b1
hr0nix <boris.jangel@gmail.com>
2015-09-26 00:53:33 +0300

    check vote_until in get_upcoming_events

--- test_wrapper.py
+++ test_wrapper.py
@@ -4,5 +4,4 @@ import db_wrapper
 
 db_wrapper.init('188.166.85.96', 27017, None, None)
 
-print db_wrapper.User(31337).get_leaderbord_index()
-print db_wrapper.User(31338).get_leaderbord_index()
+print db_wrapper.Event.get_upcoming_events()
