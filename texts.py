4517ae93d90a95ae17f87ff434ffb16818677400
Konstantin Shulgin <konstantin.shulgin@gmail.com>
2015-10-23 13:42:29 +0300

    add bot messages

---
+++ texts.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+TEXTS = {
+    "top_share_stats": "Share your stats",
+    "top_leaderboard_header": "Leaderboard ({} people total)",
+
+    "vote_prompt": "Guess the score for the game {} on {} {}. Type the score you expect, like 3:2 or 0-1.",
+    "vote_accept_prediction": "Your guess is {}:{}. You’ll know how well you did right after the end of the match on {} {}. Here’s what other people think:\n{}",
+    "vote_invalid_prediction": "{} I expect something like 3:2 or 0-1.",
+    "vote_already_voted": "You’ve already voted {}. You cannot change your vote😛 You can vote again after the next match on {} {}",
+    "vote_already_started": "Sorry, the event has already started",
+
+    "help": """Make predictions about Fenerbahçe match results and earn points:
+    /vote to make a prediction.
+
+    Compete for top positions in the leaderboard 🏆:
+    /stats to see your score and /top to see who’s the best 👑 right now.
+
+    Compete with friends 👨‍👩‍👨‍:
+    Create a group chat with your friends, invite @tahminetbot to the group, make predictions and see which one among you has the best guess.""",
+
+    "user_stat_result_good": "👯 You knew it! 👯",
+    "user_stat_result_ok": "👋 You’ve almost got it! 👋",
+    "user_stat_result_bad": "😔 Hope you’ll do better next time! 😔",
+    "user_stat_prediction": "Your prediction was {}:{}",
+    "user_stat_score": "Total score: {} points (+{})",
+    "user_stat_rating": "Current rating — {} of {}",
+
+    "group_stat_result": "Last match {} {} {} {} {} was on {} {}",
+    "group_stat_prediction": "{}. {} {} {}: voted {} +{} points ({} total)",
+}
